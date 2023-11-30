4bace54c71189ffeea3d2963bd90b01cb568e781
Metehan Selvi <me@metehanselvi.com>
2023-11-30 22:23:09 +0300

    1.1.0 - rofi-tdk

--- src/commands/rofiTdk.js
+++ src/commands/rofiTdk.js
@@ -2,23 +2,28 @@ const TMP = path.join(CACHE, 'rofi-tdk')
 
 const escape = t => t.replace(/\n/, '\\n')
 function serializeWord(word) {
-    var data = ''
+    var data = '', hasAlternative = {}, alternativeId = {}
+    for (const { madde } of word) {
+        if (hasAlternative[madde] == undefined) hasAlternative[madde] = false
+        else { hasAlternative[madde] = true; alternativeId[madde] = 1 }
+    }
     for (const madde of word) {
-        data +=  `m ${madde.madde}\nt ${madde.on_taki}\nl ${madde.lisan}\nb ${madde.birlesikler}\n`
+        if (hasAlternative[madde.madde]) { data += `k${alternativeId[madde.madde]}\n`; alternativeId[madde.madde] ++ }
+        data +=  `t${madde.taki ?? ''}\nm${madde.madde}\nl${madde.lisan ?? ''}\nb${madde.birlesikler ?? ''}\n`
+        if (madde.atasozu) data += `s${madde.atasozu.map(m => m.madde).join('|')}\n`
         if (!madde.anlamlarListe) continue
         for (const anlam of madde.anlamlarListe) {
-            data += ` a ${escape(anlam.anlam)}\n` 
-
             if (!anlam.ozelliklerListe) anlam.ozelliklerListe = []
             if (+anlam.fiil) anlam.ozelliklerListe.push({ tam_adi: 'fiil' })
-            if (anlam.ozelliklerListe.length) data += `  ${anlam.ozelliklerListe.map(v => v.tam_adi).join(', ')}\n`
+            if (anlam.ozelliklerListe.length) data += `z${anlam.ozelliklerListe.map(v => v.tam_adi).join(', ')}\n`
+            data += `a${escape(anlam.anlam)}\n` 
+
             if (anlam.orneklerListe) { 
                 for (const ornek of anlam.orneklerListe) {
-                    data += ` o ${ornek.ornek}\n` 
-                    if (ornek.yazar) data += `  ${ornek.yazar[0].tam_adi}\n`
+                    if (ornek.yazar) data += `y${ornek.yazar[0].tam_adi}\n`
+                    data += `o${ornek.ornek}\n` 
                 }
             }
-
         }
         data += '\n'
     }
